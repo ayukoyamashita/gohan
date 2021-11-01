548a137df1f525f08f854e5faee21bdfb3338800
山下彩友子 <yamashita-ayuko@s-online.co.jp>
2021-11-01 20:23:54 +0900

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,103 @@
 <template>
-  <div id="app">
-    <p>{{status}}</p>
-    <div v-for="(question, i) in questions" :key="question.id">
-      <p>{{question.label}}</p>
-      <label v-for="option in question.options" :key="option.id">
-        <input type="radio" :name="'q' + question.id" :value="option.id" v-model="answers[i]">{{option.label}}</label>
-    </div>
-    <button @click="osusumeGohan">おすすめごはん</button>
-    <p>oily:{{myScore.oily}}</p>
-    <p>heat:{{myScore.heat}}</p>
-    <p>salty:{{myScore.salty}}</p>
-    <p>sweetness:{{myScore.sweetness}}</p>
-    <p>solid:{{myScore.solid}}</p>
-    <ul>
-      <li v-for="osusume in osusumeList" :key="osusume.id">{{osusume.name}}</li>
-    </ul>
-  </div>
+	<div id="app">
+		<h1>おすすめごはん</h1>
+		<p v-if="status === 'loading'">loading</p>
+		<div v-if="status !== 'loading'">
+			<div v-for="(question, i) in questions" :key="question.id">
+				<p>{{ question.label }}</p>
+				<label v-for="option in question.options" :key="option.id">
+					<input
+						type="radio"
+						:name="'q' + question.id"
+						:value="option.id"
+						v-model="answers[i]"
+					/>{{ option.label }}</label
+				>
+			</div>
+
+			<button @click="osusumeGohan">おすすめ計算</button>
+		</div>
+		<div v-if="status === 'result'">
+			<radarchart
+				:score="[
+					myScore.oily,
+					myScore.heat,
+					myScore.salty,
+					myScore.sweetness,
+					myScore.solid,
+				]"
+			/>
+
+			<ul>
+				<li v-for="osusume in osusumeList" :key="osusume.id">
+					{{ osusume.name }}
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
-import Question from './Question'
-import Gohan from './Gohan'
+import Question from "./Question";
+import Gohan from "./Gohan";
 import axios from "axios";
-
-let q = new Question();
-console.log(Question.data);
-console.log(q.createCombination());
+import Radarchart from "./components/Radarchart";
 
 let gohan;
 
 export default {
-  name: 'App',
-  data() {
-    return {
-    status: 'ロード中',
-    questions: Question.data,
-    menuList: [],
-    myScore: {oily: 0, heat: 0, salty: 0, sweetness: 0, solid: 0},
-    osusumeList: [],
-    answers: [],
-    }
-  },
-  mounted() {
-    gohan = new Gohan();
-    this.init();
-  },
-  methods: {
-    init() {
-      axios.get("https://script.google.com/a/macros/s-online.co.jp/s/AKfycbwvrRsYMUM52D3RCpa_OwobeAxDLtigaXcPWOucIn1rrgqTLFWsJrbofXBhGNrmnzicdQ/exec"
-			).then((response) => {
-				// handle success
-				this.menuList = response.data;
-        this.status = 'ロード完了';
-        gohan.setMenuList(this.menuList);
-			}).catch(function(error) {
-				// handle error
-				console.log(error);
-			}).then(function() {
-				// always executed
-			});
-    },
-    osusumeGohan() {
-      gohan.setAnswer(this.answers);
-      this.myScore = gohan.answerScore();
-      this.osusumeList = gohan.osusume();
-    }
-  }
-}
+	name: "App",
+	components: {
+		radarchart: Radarchart,
+	},
+	data() {
+		return {
+			status: "loading",
+			questions: Question.data,
+			menuList: [],
+			myScore: { oily: 0, heat: 0, salty: 0, sweetness: 0, solid: 0 },
+			osusumeList: [],
+			answers: [],
+		};
+	},
+	mounted() {
+		gohan = new Gohan();
+		this.init();
+	},
+	methods: {
+		init() {
+			axios
+				.get(
+					"https://script.google.com/a/macros/s-online.co.jp/s/AKfycbwvrRsYMUM52D3RCpa_OwobeAxDLtigaXcPWOucIn1rrgqTLFWsJrbofXBhGNrmnzicdQ/exec"
+				)
+				.then((response) => {
+					// handle success
+					this.menuList = response.data;
+					this.status = "start";
+					gohan.setMenuList(this.menuList);
+				})
+				.catch(function(error) {
+					// handle error
+					console.log(error);
+				})
+				.then(function() {
+					// always executed
+				});
+		},
+		osusumeGohan() {
+			gohan.setAnswer(this.answers);
+			this.myScore = gohan.answerScore();
+			this.osusumeList = gohan.osusume();
+			this.status = "result";
+		},
+	},
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  
+	width: 100%;
+	max-width: 500px;
+	margin: auto;
 }
 </style>
---
+++ src/components/Radarchart.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="radar">
+		<radar-chart :chart-data="datacollection" :options="options" />
+	</div>
+</template>
+<script>
+import RadarChart from "./Radar";
+
+export default {
+	components: {
+		RadarChart,
+	},
+	props: ["score"],
+	data() {
+		return {
+			options: {
+				legend: {
+					display: false,
+				},
+				scale: {
+					ticks: {
+						min: 0,
+						max: 10,
+						stepSize: 1,
+					},
+				},
+			},
+		};
+	},
+	computed: {
+		datacollection() {
+			return {
+				labels: ["油っぽさ", "熱さ", "塩味", "甘味", "固体"],
+				datasets: [
+					{
+						data: this.score,
+					},
+				],
+			};
+		},
+	},
+	mounted() {},
+};
+</script>
+
+<style scoped>
+.radar {
+	width: 400px;
+}
+</style>
